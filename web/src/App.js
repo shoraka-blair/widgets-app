e9e1532d744f73232444de6e846e179447a309d9
Tom Wilson <tom@jackhq.com>
2017-03-29 11:12:19 -0400

    added widget to database

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
+import Widgets from './pages/index'
 import WidgetForm from './pages/form'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <header className="pa4 bg-blue white-60">
-          <h1>Boat Parts Shop</h1>
-        </header>
-        <main className="pa4">
-          <WidgetForm />
-        </main>
-      </div>
+      <BrowserRouter>
+        <div>
+          <header className="pa4 bg-blue white-60">
+            <h1>Boat Parts Shop</h1>
+            <nav>
+              <Link className="white" to="/">Widgets</Link>
+              |
+              <Link className="white" to="/new">New Widget</Link>
+            </nav>
+          </header>
+          <main className="pa4">
+            <Route exact path="/" component={Widgets} />
+            <Route path="/new" component={WidgetForm} />
+
+          </main>
+        </div>
+      </BrowserRouter>
     );
   }
 }
